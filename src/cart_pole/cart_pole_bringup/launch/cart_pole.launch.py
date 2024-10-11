d7bfe3c46be84642eda1f37eadbc5e88fbd1fe5f
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-10-11 20:00:09 +0000

    Add cart as initial version opf cart pole

---
+++ src/cart_pole/cart_pole_bringup/launch/cart_pole.launch.py
@@ -0,0 +1,57 @@
+import os
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.substitutions import Command
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    gazebo = IncludeLaunchDescription(
+        os.path.join(get_package_share_directory("ros_gz_sim"), "launch", "gz_sim.launch.py"),
+        launch_arguments=[("gz_args", ["-r -v 4 empty.sdf"])],
+    )
+
+    gazebo_spawn_robot = Node(
+        package="ros_gz_sim",
+        executable="create",
+        name="spawn_cart_pole",
+        arguments=["-name", "spawn_cart_pole", "-topic", "robot_description"],
+        output="screen",
+    )
+
+    xacro_file = os.path.join(get_package_share_directory('cart_pole_bringup'), 'urdf', 'cart_pole.urdf.xacro')
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[
+            {'robot_description': Command(['xacro ', xacro_file]) },
+            {'use_sim_time' : True}
+
+        ],
+    )
+
+    # robot_controllers = os.path.join(get_package_share_directory('cart_pole_bringup'), 'config', 'cart_pole_controllers.yaml')
+
+    joint_state_broadcaster_spawner = ExecuteProcess(
+        name="joint_state_broadcaster_spawner",
+        cmd=["ros2", "control", "load_controller", "--set-state", "active", "joint_state_broadcaster"],
+        shell=False,
+        output="screen",
+    )
+
+    velocity_controller_spawner = ExecuteProcess(
+        name="velocity_controller_spawner",
+        cmd=[ "ros2", "control", "load_controller", "--set-state", "active", "velocity_controllers"],
+        shell=False,
+        output="screen",
+    )
+
+    return LaunchDescription([
+        gazebo,
+        gazebo_spawn_robot,
+        robot_state_publisher,
+        joint_state_broadcaster_spawner,
+        velocity_controller_spawner
+    ])
