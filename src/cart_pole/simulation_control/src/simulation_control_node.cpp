7842542ce32d1ca4120ef22ec7e47f66e220ac0a
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-10-28 20:52:31 +0000

    Add service template

--- src/cart_pole/simulation_control/src/simulation_control_node.cpp
+++ src/cart_pole/simulation_control/src/simulation_control_node.cpp
@@ -1,10 +1,32 @@
-#include <cstdio>
+#include <rclcpp/rclcpp.hpp>
+#include <gz/msgs/boolean.pb.h>
+#include <gz/msgs/entity.pb.h>
+#include <gz/msgs/entity_factory.pb.h>
+#include <gz/transport/Node.hh>
+#include <std_srvs/srv/empty.hpp>
 
-int main(int argc, char ** argv)
+class SimulationControlNode : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+public:
+  SimulationControlNode(const rclcpp::NodeOptions& options)
+  : rclcpp::Node("simulation_control_node", options)
+  {
+    server_ = create_service<std_srvs::srv::Empty>("restart_sim_service", [&](
+      std_srvs::srv::Empty::Request::SharedPtr req,
+      std_srvs::srv::Empty::Response::SharedPtr resp)
+      {
+        restart_sim(req, resp);
+      });
+  }
 
-  printf("hello world simulation_control package\n");
-  return 0;
-}
+
+  void restart_sim(std_srvs::srv::Empty::Request::SharedPtr, std_srvs::srv::Empty::Response::SharedPtr)
+  {
+
+  }
+
+  rclcpp::Service<std_srvs::srv::Empty>::SharedPtr server_;
+};
+
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE(SimulationControlNode)
