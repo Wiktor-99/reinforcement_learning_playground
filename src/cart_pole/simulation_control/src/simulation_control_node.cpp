80def242278fb641c1f0cc32316161ea9d10be0f
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-11-01 09:24:59 +0000

    Minor refactor add some const, use while instead of do while

--- src/cart_pole/simulation_control/src/simulation_control_node.cpp
+++ src/cart_pole/simulation_control/src/simulation_control_node.cpp
@@ -1,38 +1,36 @@
-#include <rclcpp/rclcpp.hpp>
 #include <gz/msgs/boolean.pb.h>
 #include <gz/msgs/entity.pb.h>
 #include <gz/msgs/entity_factory.pb.h>
-#include <gz/transport/Node.hh>
-#include <std_srvs/srv/empty.hpp>
-#include <std_msgs/msg/string.hpp>
 #include <chrono>
+#include <gz/transport/Node.hh>
 #include <optional>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <std_srvs/srv/empty.hpp>
 
 std::optional<std::string> get_robot_description_from_topic()
 {
-  std::string topic_name{"robot_description"};
+  const std::string topic_name{"robot_description"};
   std::promise<std::string> robot_description_promise;
   std::shared_future<std::string> robot_description_future(robot_description_promise.get_future());
   rclcpp::executors::SingleThreadedExecutor executor;
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr description_subs;
   auto ros2_node = std::make_shared<rclcpp::Node>("robot_description_acquire_node");
   executor.add_node(ros2_node);
-  description_subs = ros2_node->create_subscription<std_msgs::msg::String>(topic_name, rclcpp::QoS(1).transient_local(),
-    [&robot_description_promise](const std_msgs::msg::String::SharedPtr msg) {
-      robot_description_promise.set_value(msg->data);
-    });
+  const auto description_subs = ros2_node->create_subscription<std_msgs::msg::String>(
+    topic_name, rclcpp::QoS(1).transient_local(),
+    [&robot_description_promise](const std_msgs::msg::String::SharedPtr msg)
+    { robot_description_promise.set_value(msg->data); });
 
   rclcpp::FutureReturnCode future_ret;
-  do
+  while (rclcpp::ok() && future_ret != rclcpp::FutureReturnCode::SUCCESS)
   {
     RCLCPP_INFO(ros2_node->get_logger(), "Waiting messages on topic [%s].", topic_name.c_str());
     future_ret = executor.spin_until_future_complete(robot_description_future, std::chrono::seconds(1));
-  } while (rclcpp::ok() && future_ret != rclcpp::FutureReturnCode::SUCCESS);
+  }
 
   if (future_ret != rclcpp::FutureReturnCode::SUCCESS)
   {
-    RCLCPP_ERROR(
-      ros2_node->get_logger(), "Failed to get XML from topic [%s].", topic_name.c_str());
+    RCLCPP_ERROR(ros2_node->get_logger(), "Failed to get XML from topic [%s].", topic_name.c_str());
     return std::nullopt;
   }
   return robot_description_future.get();
@@ -41,13 +39,12 @@ std::optional<std::string> get_robot_description_from_topic()
 class SimulationControlNode : public rclcpp::Node
 {
 public:
-  SimulationControlNode(const rclcpp::NodeOptions& options)
-  : rclcpp::Node("simulation_control_node", options)
+  SimulationControlNode(const rclcpp::NodeOptions& options) : rclcpp::Node("simulation_control_node", options)
   {
     robot_description_ = *get_robot_description_from_topic();
-    server_ = create_service<std_srvs::srv::Empty>("restart_sim_service", [&](
-      std_srvs::srv::Empty::Request::SharedPtr,
-      std_srvs::srv::Empty::Response::SharedPtr)
+    server_ = create_service<std_srvs::srv::Empty>(
+      "restart_sim_service",
+      [&](std_srvs::srv::Empty::Request::SharedPtr, std_srvs::srv::Empty::Response::SharedPtr)
       {
         execute_gazebo_request(build_remove_request(), service_remove_);
         robot_name_ = std::string("cart_pole") + std::to_string(++counter_);
@@ -73,16 +70,15 @@ public:
     return robot_spawn_request;
   }
 
-  template<typename T>
+  template <typename T>
   void execute_gazebo_request(T request, const std::string& service_name)
   {
     gz::msgs::Boolean response;
     bool result;
     const unsigned int timeout{5000};
-    while(rclcpp::ok() and not node_.Request(service_name, request, timeout, response, result))
+    while (rclcpp::ok() and not node_.Request(service_name, request, timeout, response, result))
     {
-      RCLCPP_WARN(
-        this->get_logger(), "Waiting for service [%s] to become available ...", service_name.c_str());
+      RCLCPP_WARN(this->get_logger(), "Waiting for service [%s] to become available ...", service_name.c_str());
     }
   }
 
