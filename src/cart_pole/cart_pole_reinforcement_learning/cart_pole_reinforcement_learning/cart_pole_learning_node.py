aa109a8ad31d4218477b21aa172556f521b2c91a
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-11-05 18:25:49 +0000

    Add restart_sim_service client in the learning node

--- src/cart_pole/cart_pole_reinforcement_learning/cart_pole_reinforcement_learning/cart_pole_learning_node.py
+++ src/cart_pole/cart_pole_reinforcement_learning/cart_pole_reinforcement_learning/cart_pole_learning_node.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from cart_pole_observation_interface.msg import CartPoleObservation
 from std_msgs.msg import Float64
 import numpy as np
+from std_srvs.srv import Empty
 
 UPPER_POLE_LIMIT = 1.4
 LOWER_POLE_LIMIT = -1.4
@@ -14,6 +15,7 @@ class CartPoleReinforcementLearning(Node):
         self.observation_subscriber = self.create_subscription(
             CartPoleObservation, "observations", self.store_observation, 10
         )
+        self.simulation_reset_service_client = self.create_client(Empty, "restart_sim_service")
         self.effort_command_publisher = self.create_publisher(Float64, "effort_cmd", 10)
         self.cart_observations = [0.0, 0.0, 0.0, 0.0]
         self.is_truncated = False
@@ -29,6 +31,12 @@ class CartPoleReinforcementLearning(Node):
         self.is_truncated = np.isclose(
             self.cart_observations[2], UPPER_POLE_LIMIT, rtol=1e-05, atol=1e-08, equal_nan=False
         ) or np.isclose(self.cart_observations[2], LOWER_POLE_LIMIT, rtol=1e-05, atol=1e-08, equal_nan=False)
+
+    def restart_simulation(self):
+        while not self.simulation_reset_service_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("restart_sim_service not available, waiting again...")
+
+        return self.simulation_reset_service_client.call_async(Empty.Request())
 
 
 def main(args=None):
