5f69c3edc2938d6bfa20121bba3693adff6946ea
Jakub Delicat <delicat.kuba@gmail.com>
2024-11-12 01:16:29 +0000

    Added 010 cart pole
    Signed-off-by: Jakub Delicat <delicat.kuba@gmail.com>

--- src/cart_pole/cart_pole_reinforcement_learning/cart_pole_reinforcement_learning/cart_pole_learning_control_node.py
+++ src/cart_pole/cart_pole_reinforcement_learning/cart_pole_reinforcement_learning/cart_pole_learning_control_node.py
@@ -12,7 +12,9 @@ class CartPoleReinforcementLearning(Node):
         self.observation_subscriber = self.create_subscription(
             CartPoleObservation, "observations", self.store_observation, 10
         )
-        self.simulation_reset_service_client = self.create_client(Empty, "restart_sim_service")
+        self.simulation_reset_service_client = self.create_client(
+            Empty, "restart_sim_service"
+        )
         self.effort_command_publisher = self.create_publisher(Float64, "effort_cmd", 10)
         self.cart_observations = [0.0, 0.0, 0.0, 0.0]
         self.is_truncated = False
@@ -21,8 +23,8 @@ class CartPoleReinforcementLearning(Node):
 
     def store_observation(self, cart_pole_observation: CartPoleObservation):
         self.cart_observations[0] = cart_pole_observation.cart_position
-        self.cart_observations[1] = cart_pole_observation.cart_velocity
-        self.cart_observations[2] = cart_pole_observation.pole_angle
+        self.cart_observations[1] = cart_pole_observation.pole_angle
+        self.cart_observations[2] = cart_pole_observation.cart_velocity
         self.cart_observations[3] = cart_pole_observation.pole_angular_velocity
         self.update_simulation_status()
 
@@ -41,11 +43,25 @@ class CartPoleReinforcementLearning(Node):
 
     def update_simulation_status(self):
         self.is_truncated = np.isclose(
-            self.cart_observations[2], self.UPPER_POLE_LIMIT, rtol=1e-05, atol=1e-08, equal_nan=False
-        ) or np.isclose(self.cart_observations[2], self.LOWER_POLE_LIMIT, rtol=1e-05, atol=1e-08, equal_nan=False)
+            self.cart_observations[1],
+            self.UPPER_POLE_LIMIT,
+            rtol=1e-05,
+            atol=1e-08,
+            equal_nan=False,
+        ) or np.isclose(
+            self.cart_observations[1],
+            self.LOWER_POLE_LIMIT,
+            rtol=1e-05,
+            atol=1e-08,
+            equal_nan=False,
+        )
 
     def restart_simulation(self):
-        while not self.simulation_reset_service_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info("restart_sim_service not available, waiting again...")
+        while not self.simulation_reset_service_client.wait_for_service(
+            timeout_sec=1.0
+        ):
+            self.get_logger().info(
+                "restart_sim_service not available, waiting again..."
+            )
         future = self.simulation_reset_service_client.call_async(Empty.Request())
         rclpy.spin_until_future_complete(self, future)
