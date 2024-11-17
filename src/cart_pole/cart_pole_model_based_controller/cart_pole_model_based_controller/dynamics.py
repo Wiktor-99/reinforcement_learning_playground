be64a8272b049614a5b8698bdcef6d1a44f1d970
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-11-17 13:09:23 +0000

    Partial refactor of dynamicc calculation

--- src/cart_pole/cart_pole_model_based_controller/cart_pole_model_based_controller/dynamics.py
+++ src/cart_pole/cart_pole_model_based_controller/cart_pole_model_based_controller/dynamics.py
@@ -4,91 +4,76 @@ from sympy import init_printing
 
 
 def main():
-    t = sp.Symbol("t")  # time
+    time = sp.Symbol("t")
+    cart_position = sp.Function("x1")(time)
+    pole_angle = sp.Function("x2")(time)
+    cart_velocity = sp.Function("x3")(time)
+    pole_angular_velocity = sp.Function("x4")(time)
+    pendulums_length = sp.Symbol("l")
 
-    x1 = sp.Function("x1")(t)  # position of the cart
-    x2 = sp.Function("x2")(t)  # angle of the pendulum
-    x3 = sp.Function("x3")(t)  # velocity of the cart
-    x4 = sp.Function("x4")(t)  # angular velocity of the pendulum
+    x_position = cart_position + pendulums_length * sin(pole_angle)
+    y_position = pendulums_length * cos(pole_angle)
 
-    # x3 = sp.diff(x1(t), t) # velocity of the cart
-    # x4 = sp.diff(x2(t), t) # angular velocity of the pendulum
+    x_velocity = sp.diff(x_position, time)
+    y_velocity = sp.diff(y_position, time)
+    x_velocity = x_velocity.subs(sp.Derivative(pole_angle, time), pole_angular_velocity).subs(
+        sp.Derivative(cart_position, time), cart_velocity
+    )
+    y_velocity = y_velocity.subs(sp.Derivative(pole_angle, time), pole_angular_velocity).subs(
+        sp.Derivative(cart_position, time), cart_velocity
+    )
 
-    l = sp.Symbol("l")  # length of the pendulum
+    print(f"x_velocity={x_velocity}")
+    print(f"y_velocity={y_velocity}")
 
-    # Motion equations
+    x_accelerations = sp.diff(x_velocity, time)
+    y_accelerations = sp.diff(y_velocity, time)
+    x_accelerations = x_accelerations.subs(sp.Derivative(pole_angle, time), pole_angular_velocity).subs(
+        sp.Derivative(cart_position, time), cart_velocity
+    )
+    x_accelerations = x_accelerations.subs(sp.Derivative(pole_angle, time), pole_angular_velocity).subs(
+        sp.Derivative(cart_position, time), cart_velocity
+    )
 
-    # Positions
-    x_p = x1 + l * sin(x2)
-    y_p = l * cos(x2)
+    print(f"x_accelerations={x_accelerations}")
+    print(f"y_accelerations={y_accelerations}")
 
-    # Velocities
-    dx_p = sp.diff(x_p, t)
-    dy_p = sp.diff(y_p, t)
+    pendulum_mass_kg = sp.Symbol("m")
+    pole_inertia = sp.Symbol("Ip")
+    cart_mass_kg = sp.Symbol("M")
+    g = sp.Symbol("g")
 
-    dx_p = dx_p.subs(sp.Derivative(x2, t), x4).subs(sp.Derivative(x1, t), x3)
-    dy_p = dy_p.subs(sp.Derivative(x2, t), x4).subs(sp.Derivative(x1, t), x3)
+    velocity = (x_velocity**2 + y_velocity**2).simplify()
+    pole_kinetic_energy = (
+        sp.Rational(1, 2) * pendulum_mass_kg * velocity + sp.Rational(1, 2) * pole_inertia * pole_angular_velocity**2
+    )
+    cart_kinetic_energy = sp.Rational(1, 2) * cart_mass_kg * (cart_velocity**2)
 
-    print(f"dx_p =")
-    pprint(dx_p)
-    print(f"dy_p = ")
-    pprint(dy_p)
+    kinetic_energy = Matrix([pole_kinetic_energy, cart_kinetic_energy])
+    print(f"Kinetic energy={kinetic_energy}")
 
-    # Accelerations
-    ddx_p = sp.diff(dx_p, t)
-    ddy_p = sp.diff(dy_p, t)
+    potential_energy = Matrix([pendulum_mass_kg * g * pendulums_length * cos(pole_angle), 0])
+    print(f"Potential energy={potential_energy}")
 
-    ddx_p = ddx_p.subs(sp.Derivative(x2, t), x4).subs(sp.Derivative(x1, t), x3)
-    ddx_p = ddx_p.subs(sp.Derivative(x2, t), x4).subs(sp.Derivative(x1, t), x3)
-
-    print(f"ddx_p")
-    pprint(ddx_p)
-    print(f"ddy_p = ")
-    pprint(ddy_p)
-
-    # Energies
-    m = sp.Symbol("m")  # mass of the pendulum
-    Ip = sp.Symbol("Ip")  # inertia of the pendulum
-    M = sp.Symbol("M")  # mass of the cart
-    g = sp.Symbol("g")  # gravity
-
-    v = dx_p**2 + dy_p**2
-    v = v.simplify()
-    print(f"v = ")
-    pprint(v)
-
-    # Kinetic energy
-    Tm = sp.Rational(1, 2) * m * v + sp.Rational(1, 2) * Ip * x4**2
-    TM = sp.Rational(1, 2) * M * (x3**2)
-
-    T = Matrix([Tm, TM])
-    print(f"T = ")
-    pprint(T)
-
-    # Potential energy
-    V = Matrix([m * g * l * cos(x2), 0])
-    print(f"V = =")
-    pprint(V)
-    print()
-
-    # Lagrangian
-    L = T[0] + T[1] - V[0] - V[1]
-
-    L = L.subs(x3, sp.Derivative(x1, t)).subs(x4, sp.Derivative(x2, t))
-    print(f"L = {L}")
+    lagrangian = kinetic_energy[0] + kinetic_energy[1] - potential_energy[0] - potential_energy[1]
+    lagrangian = lagrangian.subs(cart_velocity, sp.Derivative(cart_position, time)).subs(
+        pole_angular_velocity, sp.Derivative(pole_angle, time)
+    )
+    print(f"Lagrangian = {lagrangian}")
 
     # Euler-Lagrange equations
-    q = Matrix([x1, x2])
-    dq = Matrix([diff(x1, t), diff(x2, t)])
+    q = Matrix([cart_position, pole_angle])
+    dq = Matrix([diff(cart_position, time), diff(pole_angle, time)])
+    dlagrangian_dx = lagrangian.diff(q)
+    dlagrangian_ddx = lagrangian.diff(dq)
+    dlagrangian_ddx_dt = diff(dlagrangian_ddx, time)
 
-    dL_dx = L.diff(q)
-    dL_ddx = L.diff(dq)
-    dL_ddx_dt = diff(dL_ddx, t)
+    E_Le = (
+        simplify(dlagrangian_ddx_dt - dlagrangian_dx)
+        .subs(sp.Derivative(cart_position, time), cart_velocity)
+        .subs(sp.Derivative(pole_angle, time), pole_angular_velocity)
+    )
 
-    print(f"dL_dx = {dL_dx}")
-
-    E_Le = simplify(dL_ddx_dt - dL_dx).subs(sp.Derivative(x1, t), x3).subs(sp.Derivative(x2, t), x4)
-    init_printing()
     print(f"\n\nE_Le = ")
     pprint(E_Le)
     QCD = E_Le
@@ -99,18 +84,20 @@ def main():
     print(f"shape(QCD) = {QCD.shape}")
     QCD = QCD.expand()
 
-    ddq1_coeff = Matrix([QCD[0].coeff(x3.diff(t)), QCD[1].coeff(x3.diff(t))])
-    ddq2_coeff = Matrix([QCD[0].coeff(x4.diff(t)), QCD[1].coeff(x4.diff(t))])
+    ddq1_coeff = Matrix([QCD[0].coeff(cart_velocity.diff(time)), QCD[1].coeff(cart_velocity.diff(time))])
+    ddq2_coeff = Matrix(
+        [QCD[0].coeff(pole_angular_velocity.diff(time)), QCD[1].coeff(pole_angular_velocity.diff(time))]
+    )
     Q = Matrix([ddq1_coeff.transpose(), ddq2_coeff.transpose()]).transpose()
     print(f"Q = ")
     pprint(Q)
 
-    dq1_coeff = Matrix([QCD[0].coeff(x3**2), QCD[1].coeff(x3**2)])
-    dq2_coeff = Matrix([QCD[0].coeff(x4**2), QCD[1].coeff(x4**2)])
+    dq1_coeff = Matrix([QCD[0].coeff(cart_velocity**2), QCD[1].coeff(cart_velocity**2)])
+    dq2_coeff = Matrix([QCD[0].coeff(pole_angular_velocity**2), QCD[1].coeff(pole_angular_velocity**2)])
 
-    ddq = Matrix([x3.diff(t), x4.diff(t)])
-    dq = Matrix([[x3], [x4]])
-    q = Matrix([x1, x2])
+    ddq = Matrix([cart_velocity.diff(time), pole_angular_velocity.diff(time)])
+    dq = Matrix([[cart_velocity], [pole_angular_velocity]])
+    q = Matrix([cart_position, pole_angle])
 
     C = Matrix([dq1_coeff.transpose(), dq2_coeff.transpose()]).transpose() * dq
     C = Matrix([[sp.Rational(0), C[0]], [sp.Rational(0), C[1]]])
@@ -122,7 +109,7 @@ def main():
     print(f"Dq = ")
     pprint(Dq)
 
-    q_dot = Matrix([[x3], [x4]])
+    q_dot = Matrix([[cart_velocity], [pole_angular_velocity]])
 
     uF = sp.Symbol("uF")  # force applied to the cart
     u = Matrix([[uF], [0]])
@@ -136,13 +123,27 @@ def main():
     print("f(x, u) = ")
     pprint(fxu)
 
-    x = Matrix([x1, x2, x3, x4])
-    A = fxu.jacobian(x).subs(uF, 0).subs(x1, 0).subs(x2, 0).subs(x3, 0).subs(x4, 0)
+    x = Matrix([cart_position, pole_angle, cart_velocity, pole_angular_velocity])
+    A = (
+        fxu.jacobian(x)
+        .subs(uF, 0)
+        .subs(cart_position, 0)
+        .subs(pole_angle, 0)
+        .subs(cart_velocity, 0)
+        .subs(pole_angular_velocity, 0)
+    )
 
     print("A = ")
     pprint(A)
 
-    B = fxu.diff(uF).subs(uF, 0).subs(x1, 0).subs(x2, 0).subs(x3, 0).subs(x4, 0)
+    B = (
+        fxu.diff(uF)
+        .subs(uF, 0)
+        .subs(cart_position, 0)
+        .subs(pole_angle, 0)
+        .subs(cart_velocity, 0)
+        .subs(pole_angular_velocity, 0)
+    )
     print("B = ")
     pprint(B)
 
